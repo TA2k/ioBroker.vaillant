0494743fea7a47caf0ec178551358c975303e318
TA2k <tombox2020@gmail.com>
2020-01-30 21:50:58 +0100

    typo

--- main.js
+++ main.js
@@ -139,10 +139,7 @@ class Vaillant extends utils.Adapter {
         this.serialNr = "";
         this.smartPhoneId = this.makeid();
         this.isSpineActive = true;
-        if (this.config.interval < 5) {
-            this.log.warn("Interval under 5min is not recommended. Set it back to 5min");
-            this.config.interval = 5;
-        }
+
         // "multimatic_xaTaFEDoEPgAXO0HmFSMeCr5kOT6LqZoQh4LTivdW4b8HncRlKJLtExwNqjaBY1ZPnYGZPGt60NNjim0zk6tl6imL77WZ2eSdEFatxlNFT5hZkdloAL8lstiBxjqNlr5pygs9JNrlcJoTrrX0sPoqLCgE7RTn35Ok77vfX9PA3T5sa3Eqph42wz9nWaZSlcC5UsbC1ooay";
     }
 
@@ -151,7 +148,10 @@ class Vaillant extends utils.Adapter {
      */
     async onReady() {
         // Initialize your adapter here
-
+        if (this.config.interval < 5) {
+            this.log.warn("Interval under 5min is not recommended. Set it back to 5min");
+            this.config.interval = 5;
+        }
         // Reset the connection indicator during startup
         this.setState("info.connection", false, true);
         this.login()
